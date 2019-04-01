acc8dc19c14744539e4a9244648c7d67cb65dc92
KABRAASHU <40461190+KABRAASHU@users.noreply.github.com>
2019-04-01 23:47:13 +0530

    Update College.h

--- Classes/College.h
+++ Classes/College.h
@@ -1,1 +1,12 @@
-
+class college{
+	protected:
+    string college_name;
+    int college_code;
+    public:
+    college(string cn,int cd,int pref){
+        c_n = cn;
+        c_c = cd;
+    }
+    int college_code(){return c_c;}
+    string college_name(){return c_n;}
+}
