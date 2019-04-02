1d99f365430b2274d89840a65daebe027ba14081
Gautam <gautam@mishragautam.com>
2019-04-03 00:13:45 +0530

    Start allotment

--- Classes/Allocation.h
+++ Classes/Allocation.h
@@ -1,6 +1,39 @@
 //Gale–Shapley algorithm
 
+class EligibleStudent
+{
+  protected:
+    string rollNo;
+    int AIR;
+    string category;
+    vector<int> preference;
 
-void Allocate(){
+    EligibleStudent()
+    {
+    }
 
+    EligibleStudent(string roll, string air, string cat, vector<int> pref) : preference(pref)
+    {
+        rollNo = rollNo;
+        AIR = stoi(air);
+        category = cat;
+    }
+};
+
+vector<EligibleStudent> EligibleStudents;
+
+void ReadStudents()
+{
+    ifstream in;
+    Student student;
+    string StudentfileName = "Records/Student.dat";
+    in.open(StudentfileName.c_str(), ios::binary);
+    while (in.read((char *)&student, sizeof(student)))
+    {
+    }
+    in.close();
+}
+
+void Allocate()
+{
 }
--- Classes/Program.h
+++ Classes/Program.h
@@ -1,12 +1,12 @@
- class Program : public college 
+class Program : public college
 {
-  protected:
+  public:
 	string capacity;
 	int Capacity;
 	int OBC_seats;
 	int SC_ST_seats;
 	string program_code;
-  public:
+	
 	void Enter()
 	{
 		cout << "\t\t\tEnter College Name:";
@@ -22,22 +22,21 @@
 		program_code = college_code + branch_code;
 		cout << endl;
 		Capacity = stoi(capacity);
-		OBC_seats = Capacity/4;
-		SC_ST_seats = Capacity/4;
-		
+		OBC_seats = Capacity / 4;
+		SC_ST_seats = Capacity / 4;
 	}
 	void Display()
 	{
-		cout <<"\n\t\t\t          COLLEGE DETAILS" << endl;
+		cout << "\n\t\t\t          COLLEGE DETAILS" << endl;
 		cout << "\t||---------------------------------------------------------||" << endl;
-		cout << "\t College name:\t" << college_name<< endl;
-		cout << "\t Branch:\t" << branch<< endl;
-		cout << "\t General seats:\t" << Capacity/2<< endl;
-		cout << "\t OBC seats:\t" << OBC_seats<< endl;
-		cout << "\t SC/ST seats:\t" << SC_ST_seats<< endl;
+		cout << "\t College name:\t" << college_name << endl;
+		cout << "\t Branch:\t" << branch << endl;
+		cout << "\t General seats:\t" << Capacity / 2 << endl;
+		cout << "\t OBC seats:\t" << OBC_seats << endl;
+		cout << "\t SC/ST seats:\t" << SC_ST_seats << endl;
 	}
-	int getProgramID(){return stoi(program_code);}
-	int Total_seats(){return Capacity;}
-	int obc_seats(){return OBC_seats;}
-	int SCST_seats(){return SC_ST_seats;}
+	int getProgramID() { return stoi(program_code); }
+	int Total_seats() { return Capacity; }
+	int obc_seats() { return OBC_seats; }
+	int SCST_seats() { return SC_ST_seats; }
 };
