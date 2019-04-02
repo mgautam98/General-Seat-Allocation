c2ca5ddf024f2a5ecfb3025d79d83b25561ec542
KABRAASHU <40461190+KABRAASHU@users.noreply.github.com>
2019-04-02 22:31:30 +0530

    Update College.h

--- Classes/College.h
+++ Classes/College.h
@@ -3,11 +3,11 @@ class college{
 protected:
     string college_name;
     string college_code;
+    string branch;
+    string branch_code;
 public:
-    college(string cn,string cd){
-        college_name = cn;
-        college_code = cd;
-    }
+    string branchcode(){return branch_code;}
+    string branch(){return branch;}
     string collegecode(){return college_code;}
     string collegename(){return college_name;}
 }
